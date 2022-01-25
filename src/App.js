01f53b9c7b91aaef3e24a1f686916aa3d6ba2b66
michelearias <66805958+michelearias@users.noreply.github.com>
2022-01-25 09:05:59 -0300

    inclusão aula007

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import Header from './componentes/header'
 import Corpo from './componentes/corpo'
-import Dados from './componentes/dados'
- 
+
 
 export default function App(){
-
-  const cnl='CFB Cursos'
   
     return(
       <>
       <Header/>
       <Corpo/>
-      <Dados canal={cnl} youtube='youtube.com/cfbcursos' curso='React.js'/>
+      
       </>
 
     )
--- src/componentes/corpo.js
+++ src/componentes/corpo.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Dados from './dados'
 
 export default function Corpo(){
+    const cnl=()=>{
+        return 'CFB Cursos'
+    }
+
+    const yt='youtube.com/CFB Cursos'
+
     return(
+        
     <section>
+        
         <h2>Curso de React</h2>
         <p>Se inscreva em nosso canal!</p>
         <p>Ative as notificações!</p>
+        <Dados canal={cnl} youtube={yt} curso='React.js'/>
     </section>
     )
 }
